fcc21ceec8be911fc317c27d9fc82c3a73eb5577
Tom Vaillant <tom.vaillant.vt@gmail.com>
2025-10-14 16:16:22 +0200

    honeypot anti-spam

--- scripts/email.js
+++ scripts/email.js
@@ -4,10 +4,19 @@
     document.getElementById('contact-form').addEventListener('submit', function (event) {
         event.preventDefault();
 
+        // verif honeypot, si rempli > bot
+        const honeypot = document.getElementById('website').value;
+        if (honeypot) {
+            console.log('Bot détecté - soumission bloquée');
+            this.reset();
+            return;
+        }
+
+        // si honeypot vide > humain > envoi email
         emailjs.sendForm('service_iqs4i5t', 'template_vcdkvbj', this)
             .then(() => {
                 alert('E-mail envoyé avec succès !');
-                this.reset(); // Réinitialiser le formulaire après envoi
+                this.reset(); // reset form après envoi
             })
             .catch((err) => {
                 console.error('Erreur lors de l\'envoi :', err);
